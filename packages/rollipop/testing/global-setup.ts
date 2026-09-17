e7dbd13973f2d0fd1ae40986a4fabcad1b06dfda
leegeunhyeok <dev.ghlee@gmail.com>
2026-09-17 17:18:48 +0900

    test: enable slient option

--- packages/rollipop/testing/global-setup.ts
+++ packages/rollipop/testing/global-setup.ts
@@ -1,19 +1,15 @@
-import { execSync } from 'node:child_process';
-import path from 'node:path';
+import { execSync, type ExecSyncOptionsWithStringEncoding } from 'node:child_process';
+
+const EXEC_OPTIONS: ExecSyncOptionsWithStringEncoding = {
+  // https://yarnpkg.com/advanced/lifecycle-scripts#environment-variables
+  cwd: process.env.PROJECT_CWD,
+  timeout: 300_000,
+  encoding: 'utf-8',
+};
 
 export function setup() {
-  const monorepoRoot = path.resolve(import.meta.dirname, '..', '..', '..');
-
-  console.log('Building workspace packages...');
-  execSync('yarn build:all', {
-    cwd: monorepoRoot,
-    stdio: 'inherit',
-    timeout: 300_000,
-  });
-  execSync('yarn workspace example-0.84 devtools:build', {
-    cwd: monorepoRoot,
-    stdio: 'inherit',
-    timeout: 300_000,
-  });
-  console.log('Build complete.');
+  console.log('Setting up test environment...');
+  execSync('yarn build:all', EXEC_OPTIONS);
+  execSync('yarn workspace example-0.84 devtools:build', EXEC_OPTIONS);
+  console.log();
 }
